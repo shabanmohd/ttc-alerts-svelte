fc82f17eaff50b676e14e9f44a7dd2f7431c91d5
shabanmohd <bezykids@gmail.com>
2026-01-11 18:03:13 -0500

    style: restore light mode bottom nav bg fill to original HSL value

--- static/sw.js
+++ static/sw.js
@@ -8,7 +8,7 @@ const IMMUTABLE_CACHE = 'ttc-immutable-v6';
 
 // SW Version for logging and debugging
 const SW_VERSION = '9.0.0';
-const BUILD_TIMESTAMP = '2026-01-11T22:59:37.153Z';
+const BUILD_TIMESTAMP = '2026-01-11T23:02:46.849Z';
 
 const STATIC_ASSETS = [
   '/',
